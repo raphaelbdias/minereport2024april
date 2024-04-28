98d66195d107031e80a783905428e896c3b1a7e4
Raphael B. Dias <86736451+raphaelbdias@users.noreply.github.com>
2024-04-28 18:47:53 +0000

    changtes

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -21,4 +21,20 @@ document.addEventListener("DOMContentLoaded", function() {
 
     document.addEventListener('scroll', toggleSections);
     toggleSections(); // Initial check
+
+    // Collapsible functionality
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.maxHeight){
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+            }
+        });
+    }
 });
